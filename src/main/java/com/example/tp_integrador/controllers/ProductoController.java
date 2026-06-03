32ffbcbe8bc2dce8ed1ab488757ba4a6a7c09dd5
Saul Hillar <saulhillar@outlook.es>
2026-06-03 08:37:55 -0300

    feat: add basic methods to product controller

--- src/main/java/com/example/tp_integrador/controllers/ProductoController.java
+++ src/main/java/com/example/tp_integrador/controllers/ProductoController.java
@@ -1,11 +1,45 @@
 package com.example.tp_integrador.controllers;
 
+import com.example.tp_integrador.dtos.producto.ProductoCreate;
+import com.example.tp_integrador.dtos.producto.ProductoDto;
+import com.example.tp_integrador.dtos.producto.ProductoEdit;
+import com.example.tp_integrador.services.ProductoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/productos")
 @RequiredArgsConstructor
 public class ProductoController {
+
+    private final ProductoService productoService;
+
+    @GetMapping("/all")
+    public void findAll() {
+        productoService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public void findById(@PathVariable Long id) {
+        productoService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductoDto> update(@Valid @PathVariable Long id, @Valid @RequestBody ProductoEdit productoEdit) {
+        ProductoDto prodcutoDto = productoService.update(productoEdit, id);
+        return ResponseEntity.ok(prodcutoDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@Valid @PathVariable Long id) {
+        productoService.delete(id);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ProductoDto> create(@Valid @RequestBody ProductoCreate productoCreate) {
+        ProductoDto productoDto = productoService.save(productoCreate);
+        return ResponseEntity.ok(productoDto);
+    }
 }
--- src/main/java/com/example/tp_integrador/dtos/producto/ProductoCreate.java
+++ src/main/java/com/example/tp_integrador/dtos/producto/ProductoCreate.java
@@ -2,13 +2,19 @@ package com.example.tp_integrador.dtos.producto;
 
 import com.example.tp_integrador.entities.Categoria;
 import com.example.tp_integrador.entities.Producto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record ProductoCreate(
+        @NotBlank
         String nombre,
+        @NotBlank
         Double precio,
         String descripcion,
+        @NotBlank @Size (min = 1)
         Integer stock,
         String imagen,
+        @NotBlank
         Long categoriaId
 ) {
     public Producto toEntity(){
