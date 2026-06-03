ebf7755d1f45cc3759556268328735ec362bc4a8
Saul Hillar <saulhillar@outlook.es>
2026-06-03 09:27:48 -0300

    feat: add user controller with basic methods

--- src/main/java/com/example/tp_integrador/controllers/UsuarioController.java
+++ src/main/java/com/example/tp_integrador/controllers/UsuarioController.java
@@ -1,11 +1,49 @@
 package com.example.tp_integrador.controllers;
 
+
+import com.example.tp_integrador.dtos.usuario.UsuarioCreate;
+import com.example.tp_integrador.dtos.usuario.UsuarioDto;
+import com.example.tp_integrador.dtos.usuario.UsuarioEdit;
+import com.example.tp_integrador.services.UsuarioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
 @RequiredArgsConstructor
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping("/create")
+    public ResponseEntity<UsuarioDto> create(@Valid @RequestBody UsuarioCreate usuarioCreate) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.save(usuarioCreate));
+    }
+
+    @GetMapping({"/{id}"})
+    public ResponseEntity<UsuarioDto> findById(@PathVariable Long id){
+        return ResponseEntity.ok(usuarioService.findById(id));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<UsuarioDto>> findAll() {
+        return ResponseEntity.ok(usuarioService.findAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioDto> update(@Valid @RequestBody UsuarioEdit usuarioEdit, @PathVariable Long id){
+        return ResponseEntity.ok(usuarioService.update(usuarioEdit, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        usuarioService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
